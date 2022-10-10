7c5a4b9ca1e67f274bf83682077dfca6447c989b
Pamela Quirós Ureña <pamelaquirosu@hotmail.com>
2022-10-10 17:52:43 -0600

    Section 3: Shuffling a Deck

--- cards/main.go
+++ cards/main.go
@@ -1,13 +1,8 @@
 package main
 
 func main() {
-	cards := newDeckFromFile("my")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
-	//cards.saveToFile("my_cards")
-
-	// hand, remainingCards := deal(cards, 5)
-
-	// hand.print()
-	// remainingCards.print()
 
 }
