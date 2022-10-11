eaa782aea0b6a00f98390c0c82e840dbe9bd3b32
Pamela Quirós Ureña <pamelaquirosu@hotmail.com>
2022-10-11 14:31:31 -0600

    Section 5: Maps

---
+++ maps/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var colors map[string]string
+	//colors := make(map[int]string)
+	//colors[10] = "#ffffff"
+	//delete(colors, 10)
+
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"whitw": "#ffffff",
+	}
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
