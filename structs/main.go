c3daec0d3e1f4a3ee4d92fafb4b050b785be8008
Pamela Quirós Ureña <pamelaquirosu@hotmail.com>
2022-10-11 12:21:24 -0600

    Section 4: Structs with Pointers

---
+++ structs/main.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	//contact   contactInfo
+	contactInfo
+}
+
+func main() {
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	// fmt.Println(alex)
+	// var alex person //empty strings
+
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jum",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+
+	fmt.Printf("%+v", p)
+}
