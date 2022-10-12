a783ab4aed8120628fe2e23f5b90e26064706b95
Pamela Quirós Ureña <pamelaquirosu@hotmail.com>
2022-10-12 10:37:24 -0600

    Section 6: Writer Interface

--- http/main.go
+++ http/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct{}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -14,7 +17,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
 	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+
+	return len(bs), nil
 }
